612c1ffa82f268d14a436ee95461d58b9c6dc39f
Dylon <edafrika07@gmail.com>
2025-07-31 15:20:17 +0200

    updated TodoCard and TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
+  let todos = [
+    'Go to the gym',
+    'Eat more fruits and veg',
+    'Do LeetCode questions'
+  ]
 
   return (
     <>
-      <div>{4 * 4}</div>
+      <>
+        <TodoInput />
+        <TodoList todos={todos} />
+      </>
     </>
   )
 }
